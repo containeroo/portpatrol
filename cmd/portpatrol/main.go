c24fe7e1602363dbecdba48ca27404a76075ea0c
gi8 <giottolino@gmail.com>
2024-09-02 22:09:26 +0200

    update version

--- cmd/portpatrol/main.go
+++ cmd/portpatrol/main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/containeroo/portpatrol/internal/runner"
 )
 
-const version = "0.4.1"
+const version = "0.4.2"
 
 // run is the main function of the application
 func run(ctx context.Context, getEnv func(string) string, output io.Writer) error {
