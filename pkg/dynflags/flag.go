7fa8c5697695416024fd63c4df4a90d45a0faf06
gi8 <giottolino@gmail.com>
2024-12-13 12:53:42 +0100

    updates

--- pkg/dynflags/flag.go
+++ pkg/dynflags/flag.go
@@ -3,20 +3,20 @@ package dynflags
 type FlagType string
 
 const (
-	FlagTypeStringSlice   FlagType = "..STRING"
+	FlagTypeStringSlice   FlagType = "..STRINGs"
 	FlagTypeString        FlagType = "STRING"
 	FlagTypeInt           FlagType = "INT"
-	FlagTypeIntSlice      FlagType = "..INT"
+	FlagTypeIntSlice      FlagType = "..INTs"
 	FlagTypeBool          FlagType = "BOOL"
-	FlagTypeBoolSlice     FlagType = "..BOOL"
+	FlagTypeBoolSlice     FlagType = "..BOOLs"
 	FlagTypeDuration      FlagType = "DURATION"
-	FlagTypeDurationSlice FlagType = "..DURATION"
+	FlagTypeDurationSlice FlagType = "..DURATIONs"
 	FlagTypeFloat         FlagType = "FLOAT"
-	FlagTypeFloatSlice    FlagType = "..FLOAT"
+	FlagTypeFloatSlice    FlagType = "..FLOATs"
 	FlagTypeIP            FlagType = "IP"
-	FlagTypeIPSlice       FlagType = "..IP"
+	FlagTypeIPSlice       FlagType = "..IPs"
 	FlagTypeURL           FlagType = "URL"
-	FlagTypeURLSlice      FlagType = "..URL"
+	FlagTypeURLSlice      FlagType = "..URLs"
 )
 
 // Flag represents a single configuration flag
@@ -29,6 +29,8 @@ type Flag struct {
 
 // FlagValue interface encapsulates parsing and value-setting logic
 type FlagValue interface {
+	// Parse parses the given string value into the flag's value type
 	Parse(value string) (interface{}, error)
+	// Set sets the flag's value to the given value
 	Set(value interface{}) error
 }
