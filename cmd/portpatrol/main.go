f78c8f315136705482ef47c1b45d3b2512983683
gi8 <giottolino@gmail.com>
2025-02-01 20:20:00 +0100

    new release

--- cmd/portpatrol/main.go
+++ cmd/portpatrol/main.go
@@ -16,7 +16,7 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-const version string = "v0.5.4"
+const version string = "v0.5.5"
 
 // run is the main function of the application.
 func run(ctx context.Context, args []string, output io.Writer) error {
